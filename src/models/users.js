5a9aa2e9762baed407b52dff541d423d701258a0
ZhouKaiDong <953343906@qq.com>
2019-08-21 18:31:18 +0800

    Users

--- src/models/users.js
+++ src/models/users.js
@@ -11,7 +11,21 @@ export default {
         editShow: false,
         detailShow: false,
         list: [],
-        query: []
+        query: [],
+        parameter:{
+            login_name:"",//账号
+            password:"",//密码
+            nick_name:"",//用户昵称
+            real_name:"",//真实姓名
+            user_type:"",//用户类型
+            email:"",//用户邮箱
+            phonenumber:"",//手机号码
+            dept_id:"",//部门ID
+            sex:"",//用户性别（0男 1女 2未知
+            avatar:"",//头像路径
+            status:"",//帐号状态（0正常 1停用）
+            remark:""//备注
+        }
     },
     effects: {
         /**
@@ -20,6 +34,7 @@ export default {
         * list({ payload }, { call, put }) {
 
             let list = yield call(queryUserList, { pageSize: 10, page: 1 });
+            console.log("请求成功",list);
             yield put({
                 type: 'updateState',
                 payload: { "list": list },
@@ -30,8 +45,8 @@ export default {
          * 增加用户
          */
         * add({ payload }, { call, put }) {
-            
-            let rs = yield call(addUser, { userName: "周开栋", sex: "男" });
+            console.log("开始发送请求",payload);
+            let rs = yield call(addUser, payload);
             console.log(rs);
             //数据提交成功，隐藏表单
             if (rs.code == 200) {
--- src/pages/users/add.js
+++ src/pages/users/add.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Icon } from 'antd';
+import { Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Icon, TreeSelect, Radio } from 'antd';
 import { connect } from 'dva';
+import Onepic from '../../components/upload/Onepic';
 const { Option } = Select;
 
 //连接model,并将addShow赋值给props
@@ -34,23 +35,38 @@ class Add extends React.PureComponent {
   };
 
   saveData = () => {
-    console.log("新增用户");
+    console.log("新增用户",this.props.form);
     const { dispatch } = this.props;
-    dispatch({
-      type: 'users/add',
-      payload: {userName: "周开栋", sex: "男"},
+    //验证表单数据
+    this.props.form.validateFields((errors, values) => {
+      if (errors) {
+          console.log('Errors in form!!!');
+          return;
+      }
+      
+      //所有的form表单值，等同于values
+      const formData = this.props.form.getFieldsValue();
+      dispatch({
+        type: 'users/add',
+        payload: formData
+      });
+
     });
+   
   };
 
+  deptChange = value => {
+    console.log(value);
+  };
 
   render() {
     const { addShow } = this.props;
-    const { saveData,onClose } = this;
+    const { saveData, onClose } = this;
     const { getFieldDecorator } = this.props.form;
     return (
       <div>
         <Drawer
-          title="Create a new account"
+          title="创建用户"
           width={720}
           onClose={this.onClose}
           visible={addShow}
@@ -58,90 +74,128 @@ class Add extends React.PureComponent {
           <Form layout="vertical" hideRequiredMark>
             <Row gutter={16}>
               <Col span={12}>
-                <Form.Item label="Name">
-                  {getFieldDecorator('name', {
-                    rules: [{ required: true, message: 'Please enter user name' }],
-                  })(<Input placeholder="Please enter user name" />)}
+                <Row gutter={16}>
+                  <Form.Item label="用户类型" hasFeedback>
+                    {getFieldDecorator('user_type', {
+                      rules: [{ required: true, message: '请选择用户类型' }],
+                    })(
+                      <Select placeholder="请选择用户类型">
+                        <Option value="100">系统管理员</Option>
+                        <Option value="200">普通用户</Option>
+                      </Select>
+                    )}
+                  </Form.Item>
+                </Row>
+                <Row gutter={16}>
+                  <Form.Item label="所属部门">
+                    {getFieldDecorator('dept_id', {
+                      rules: [{ message: '请选择所属部门' }],
+                    })(
+                      <TreeSelect
+                        dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                        treeData={deptData}
+                        placeholder="请选择"
+                        treeDefaultExpandAll
+                        onChange={this.deptChange}
+                      />
+                    )}
+                  </Form.Item>
+                </Row>
+              </Col>
+              <Col span={12} style={{ marginTop: "40px", paddingLeft: "100px" }}>
+                <Onepic utext="上传头像"></Onepic>
+              </Col>
+
+            </Row>
+
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="登录账号">
+                  {getFieldDecorator('login_name', {
+                    rules: [{ required: true, message: '请输入登录账号' }],
+                  })(<Input placeholder="请输入登录账号" />)}
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Url">
-                  {getFieldDecorator('url', {
-                    rules: [{ required: true, message: 'Please enter url' }],
+                <Form.Item label="登录密码">
+                  {getFieldDecorator('password', {
+                    rules: [{ message: '请输入登录密码' }],
                   })(
-                    <Input
-                      style={{ width: '100%' }}
-                      addonBefore="http://"
-                      addonAfter=".com"
-                      placeholder="Please enter url"
-                    />,
+                    <Input placeholder="请输入登录密码" />
                   )}
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="用户昵称">
+                  {getFieldDecorator('nick_name', {
+                    rules: [{ message: '请输入用户昵称' }],
+                  })(<Input placeholder="请输入用户昵称" />)}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="真实姓名">
+                  {getFieldDecorator('real_name', {
+                    rules: [{ message: '请输入真实姓名' }],
+                  })(<Input placeholder="请输入真实姓名" />)}
                 </Form.Item>
               </Col>
             </Row>
             <Row gutter={16}>
               <Col span={12}>
-                <Form.Item label="Owner">
-                  {getFieldDecorator('owner', {
-                    rules: [{ required: true, message: 'Please select an owner' }],
-                  })(
-                    <Select placeholder="Please select an owner">
-                      <Option value="xiao">Xiaoxiao Fu</Option>
-                      <Option value="mao">Maomao Zhou</Option>
-                    </Select>,
-                  )}
+                <Form.Item label="邮箱">
+                  {getFieldDecorator('email', {
+                    rules: [{
+                      type: 'email',
+                      message: '这不是一个邮箱',
+                    }, { message: '请输入邮箱' }],
+                  })(<Input placeholder="请输入邮箱" />)}
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Type">
-                  {getFieldDecorator('type', {
-                    rules: [{ required: true, message: 'Please choose the type' }],
-                  })(
-                    <Select placeholder="Please choose the type">
-                      <Option value="private">Private</Option>
-                      <Option value="public">Public</Option>
-                    </Select>,
-                  )}
+                <Form.Item label="手机号码">
+                  {getFieldDecorator('phonenumber', {
+                    rules: [{ message: '请输入手机号码' }],
+                  })(<Input placeholder="请输入手机号码" />)}
                 </Form.Item>
               </Col>
             </Row>
             <Row gutter={16}>
               <Col span={12}>
-                <Form.Item label="Approver">
-                  {getFieldDecorator('approver', {
-                    rules: [{ required: true, message: 'Please choose the approver' }],
-                  })(
-                    <Select placeholder="Please choose the approver">
-                      <Option value="jack">Jack Ma</Option>
-                      <Option value="tom">Tom Liu</Option>
-                    </Select>,
+                <Form.Item label="性别">
+                  {getFieldDecorator('sex', {
+                    rules: [{ message: '请选择性别' }],
+                  })(<Radio.Group buttonStyle="solid">
+                    <Radio.Button value="0">男</Radio.Button>
+                    <Radio.Button value="1">女</Radio.Button>
+                  </Radio.Group>
                   )}
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="DateTime">
-                  {getFieldDecorator('dateTime', {
-                    rules: [{ required: true, message: 'Please choose the dateTime' }],
-                  })(
-                    <DatePicker.RangePicker
-                      style={{ width: '100%' }}
-                      getPopupContainer={trigger => trigger.parentNode}
-                    />,
-                  )}
+                <Form.Item label="帐号状态">
+                  {getFieldDecorator('status', {
+                    rules: [{ message: '请输入手机号码' }],
+                  })(<Radio.Group buttonStyle="solid">
+                  <Radio.Button value="1">正常</Radio.Button>
+                  <Radio.Button value="0">停用</Radio.Button>
+                </Radio.Group>)}
                 </Form.Item>
               </Col>
             </Row>
+            
             <Row gutter={16}>
               <Col span={24}>
-                <Form.Item label="Description">
-                  {getFieldDecorator('description', {
+                <Form.Item label="备注">
+                  {getFieldDecorator('remark', {
                     rules: [
                       {
-                        required: true,
-                        message: 'please enter url description',
+                        message: '备注',
                       },
                     ],
-                  })(<Input.TextArea rows={4} placeholder="please enter url description" />)}
+                  })(<Input.TextArea rows={4} placeholder="备注" />)}
                 </Form.Item>
               </Col>
             </Row>
@@ -174,3 +228,35 @@ class Add extends React.PureComponent {
 // ReactDOM.render(<Add />, document.getElementById('root'));
 
 export default Add;
+
+const deptData = [
+  {
+    title: '研发部',
+    value: '0-0',
+    key: '0-0',
+    children: [
+      {
+        title: 'antd研发',
+        value: '0-0-1',
+        key: '0-0-1',
+      },
+      {
+        title: 'egg研发',
+        value: '0-0-2',
+        key: '0-0-2',
+      },
+    ],
+  },
+  {
+    title: '市场部',
+    value: '0-1',
+    key: '0-1',
+    children: [
+      {
+        title: '营销策划',
+        value: '0-1-1',
+        key: '0-1-1',
+      }
+    ],
+  },
+];

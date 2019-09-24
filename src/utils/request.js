aef170ef40b113912e49de7096bd7c6b01dcdb94
ZhouKaiDong <953343906@qq.com>
2019-09-24 18:51:14 +0800

    Zkd

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,9 +3,28 @@ import { cloneDeep, isEmpty } from 'lodash'
 import pathToRegexp from 'path-to-regexp'
 import { message } from 'antd'
 import router from 'umi/router';
+import { notification } from 'antd';
 // axios.defaults.baseURL = "http://localhost:9531"
-const { CancelToken } = axios
-window.cancelRequest = new Map()
+const { CancelToken } = axios;
+window.cancelRequest = new Map();
+//定义状态码
+const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+};
 
 export default function request(options) {
   let { data, url, method = 'get' } = options
@@ -41,16 +60,37 @@ export default function request(options) {
       cancel,
     })
   })
-// console.log("axios请求",options);
+  // console.log("axios请求",options);
   return axios(options)
     .then(response => {
-      const { statusText, status, data } = response
+      const { statusText, status, data } = response;
+      const errortext = codeMessage[status] || statusText;
       //对返回status进行判断
-      console.log("请求状态=="+status);
-      if(status==401){
+      if (status == 401) {
+        notification.error({
+          message: '未登录或登录已过期，请重新登录。',
+        });
         router.push("/auth/login");
         return;
+      } else if (status != 200) {
+        notification.error({
+          message: `请求错误 ${status}: ${options.url}`,
+          description: errortext,
+        });
+        // if (status === 403) {
+        //   router.push('/exception/403');
+        //   return;
+        // }
+        // if (status <= 504 && status >= 500) {
+        //   router.push('/exception/500');
+        //   return;
+        // }
+        // if (status >= 404 && status < 422) {
+        //   router.push('/exception/404');
+        //   return;
+        // }
       }
+
       let result = {}
       if (typeof data === 'object') {
         result = data
@@ -70,7 +110,7 @@ export default function request(options) {
     })
     .catch(error => {
       const { response, message } = error
-      console.log("请求失败",error);
+      console.log("请求失败", error);
       if (String(message) === "cancel request") {
         return {
           success: false,
